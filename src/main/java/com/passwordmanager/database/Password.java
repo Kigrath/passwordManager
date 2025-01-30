723477e6ee8e8a75fab7ade7ed7c8124b4c33f3e
Kigrath <chris.martin.ck14@gmail.com>
2025-01-30 23:04:25 +0100

    Add encryption feature

--- src/main/java/com/passwordmanager/database/Password.java
+++ src/main/java/com/passwordmanager/database/Password.java
@@ -1,48 +1,45 @@
 package com.passwordmanager.database;
 
+import com.passwordmanager.encryption.EncryptionUtils;
+
 public class Password {
     private String site;
     private String username;
-    private String password;
+    private String encryptedPassword;
+    private static final String KEY = "MeinGeheimerKey!!"; // Schlüssel (MUSS 16 Zeichen lang sein!)
 
-    // Konstruktor
     public Password(String site, String username, String password) {
         this.site = site;
         this.username = username;
-        this.password = password;
+        try {
+            this.encryptedPassword = EncryptionUtils.encrypt(password, KEY);
+        } catch (Exception e) {
+            throw new RuntimeException("Fehler bei der Verschlüsselung!", e);
+        }
     }
 
-    // Getter und Setter
     public String getSite() {
         return site;
-    }
-
-    public void setSite(String site) {
-        this.site = site;
     }
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getPassword() {
-        return password;
+        try {
+            return EncryptionUtils.decrypt(encryptedPassword, KEY);
+        } catch (Exception e) {
+            throw new RuntimeException("Fehler bei der Entschlüsselung!", e);
+        }
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    // Für Debugging: Optionale Ausgabe ohne Passwort
     @Override
     public String toString() {
         return "Password{" +
                 "site='" + site + '\'' +
                 ", username='" + username + '\'' +
+                ", password='***** (verschlüsselt)' " + // Passwort nicht direkt anzeigen
                 '}';
     }
 }
--- src/main/java/com/passwordmanager/encryption/EncryptionUtils.java
+++ src/main/java/com/passwordmanager/encryption/EncryptionUtils.java
@@ -1,4 +1,25 @@
 package com.passwordmanager.encryption;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
 public class EncryptionUtils {
+    private static final String ALGORITHM = "AES";
+
+    public static String encrypt(String plainText, String key) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptedBytes = cipher.doFinal(plainText.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    public static String decrypt(String cipherText, String key) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(cipherText));
+        return new String(decryptedBytes);
+    }
 }
